6b28ca5247eb3ca5cb9b7d5bb4c18b917c7bc3d2
dany47788 <65661948+dany47788@users.noreply.github.com>
2024-02-01 15:56:16 -0300

    update strings

--- scenes/mainMenu.go
+++ scenes/mainMenu.go
@@ -13,65 +13,32 @@ func (s MainMenuScene) Start(bot *chatbot.Bot) {
 		if !util.IsSessionExpired(message) {
 			text, _ := message.Text()
 			switch text {
-
 			case "1":
-				message.UpdateStateData(map[string]interface{}{"lang": "eng"})
-				message.AnswerWithText(
-					util.GetString([]string{"welcome_message", "eng"}) +
-						message.Body["senderData"].(map[string]interface{})["senderName"].(string) + "\n" +
-						util.GetString([]string{"menu", "eng"}))
-
-				message.ActivateNextScene(EndpointsScene{})
-
+				s.sendMainMenu(message, "en")
 			case "2":
-				message.UpdateStateData(map[string]interface{}{"lang": "ru"})
-				message.AnswerWithText(
-					util.GetString([]string{"welcome_message", "ru"}) +
-						message.Body["senderData"].(map[string]interface{})["senderName"].(string) +
-						util.GetString([]string{"menu", "ru"}))
-
-				message.ActivateNextScene(EndpointsScene{})
-
+				s.sendMainMenu(message, "ru")
 			case "3":
-				message.UpdateStateData(map[string]interface{}{"lang": "kz"})
-				message.AnswerWithText(
-					util.GetString([]string{"welcome_message", "kz"}) +
-						message.Body["senderData"].(map[string]interface{})["senderName"].(string) +
-						util.GetString([]string{"menu", "kz"}))
-
-				message.ActivateNextScene(EndpointsScene{})
-
+				s.sendMainMenu(message, "kz")
 			case "4":
-				message.UpdateStateData(map[string]interface{}{"lang": "he"})
-				message.AnswerWithText(
-					message.Body["senderData"].(map[string]interface{})["senderName"].(string) +
-						util.GetString([]string{"welcome_message", "he"}) +
-						util.GetString([]string{"menu", "he"}))
-
-				message.ActivateNextScene(EndpointsScene{})
-
+				s.sendMainMenu(message, "he")
 			case "5":
-				message.UpdateStateData(map[string]interface{}{"lang": "es"})
-				message.AnswerWithText(
-					util.GetString([]string{"welcome_message", "es"}) +
-						message.Body["senderData"].(map[string]interface{})["senderName"].(string) +
-						util.GetString([]string{"menu", "es"}))
-
-				message.ActivateNextScene(EndpointsScene{})
-
+				s.sendMainMenu(message, "es")
 			case "6":
-				message.UpdateStateData(map[string]interface{}{"lang": "ar"})
-				message.AnswerWithText(
-					util.GetString([]string{"welcome_message", "ar"}) +
-						message.Body["senderData"].(map[string]interface{})["senderName"].(string) +
-						util.GetString([]string{"menu", "ar"}))
-
-				message.ActivateNextScene(EndpointsScene{})
-
+				s.sendMainMenu(message, "ar")
 			default:
 				message.AnswerWithText(
 					util.GetString([]string{"specify_language"}))
 			}
 		}
 	})
+}
+
+func (s MainMenuScene) sendMainMenu(message *chatbot.Notification, lang string) {
+	message.UpdateStateData(map[string]interface{}{"lang": lang})
+	message.AnswerWithText(
+		util.GetString([]string{"welcome_message", lang}) +
+			message.Body["senderData"].(map[string]interface{})["senderName"].(string) + "\n" +
+			util.GetString([]string{"menu", lang}))
+
+	message.ActivateNextScene(EndpointsScene{})
 }
