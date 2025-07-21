1bacd03f01442418839cc8347e92c778e196d832
prostraction <47314760+prostraction@users.noreply.github.com>
2025-07-21 13:56:03 +0300

    SW-5174

--- scenes/mainMenu.go
+++ scenes/mainMenu.go
@@ -11,30 +11,34 @@ type MainMenuScene struct {
 func (s MainMenuScene) Start(bot *chatbot.Bot) {
 	bot.IncomingMessageHandler(func(message *chatbot.Notification) {
 		if !util.IsSessionExpired(message) {
-			text, _ := message.Text()
-			switch text {
-			case "1":
-				s.sendMainMenu(message, "en")
-			case "2":
-				s.sendMainMenu(message, "kz")
-			case "3":
-				s.sendMainMenu(message, "ru")
-			case "4":
-				s.sendMainMenu(message, "es")
-			case "5":
-				s.sendMainMenu(message, "he")
-			// enable only and only when ar language will be released and ready
-			// otherwise, crash happens
-			//case "6":
-			//	s.sendMainMenu(message, "ar")
-			default:
-				message.SendText(util.GetString([]string{"specify_language"}))
-			}
+			s.SendMainMenu(message)
 		} else {
-			message.ActivateNextScene(MainMenuScene{})
+			message.ActivateNextScene(StartScene{})
 			message.SendText(util.GetString([]string{"select_language"}))
 		}
 	})
+}
+
+func (s MainMenuScene) SendMainMenu(message *chatbot.Notification) {
+	text, _ := message.Text()
+	switch text {
+	case "1":
+		s.sendMainMenu(message, "en")
+	case "2":
+		s.sendMainMenu(message, "kz")
+	case "3":
+		s.sendMainMenu(message, "ru")
+	case "4":
+		s.sendMainMenu(message, "es")
+	case "5":
+		s.sendMainMenu(message, "he")
+	// enable only and only when ar language will be released and ready
+	// otherwise, crash happens
+	//case "6":
+	//	s.sendMainMenu(message, "ar")
+	default:
+		message.SendText(util.GetString([]string{"specify_language"}))
+	}
 }
 
 func (s MainMenuScene) sendMainMenu(message *chatbot.Notification, lang string) {
@@ -50,4 +54,5 @@ func (s MainMenuScene) sendMainMenu(message *chatbot.Notification, lang string) 
 		"welcome.jpg",
 		util.GetString([]string{"welcome_message", lang})+"*"+message.Body["senderData"].(map[string]interface{})["senderName"].(string)+"*!"+"\n"+util.GetString([]string{"menu", lang}))
 	message.ActivateNextScene(EndpointsScene{})
+
 }
--- scenes/start.go
+++ scenes/start.go
@@ -10,9 +10,18 @@ type StartScene struct {
 
 func (s StartScene) Start(bot *chatbot.Bot) {
 	bot.IncomingMessageHandler(func(message *chatbot.Notification) {
-		util.IsSessionExpired(message)
-
-		message.SendText(util.GetString([]string{"select_language"}))
-		message.ActivateNextScene(MainMenuScene{})
+		if util.IsSessionExpired(message) {
+			message.SendText(util.GetString([]string{"select_language"}))
+			message.ActivateNextScene(MainMenuScene{})
+		} else {
+			// This code will be executed after idle timeout (10 minutes)
+			//  from endpoints.go or mainMenu.go.
+			// The start message is already printed from one of the file above
+			//  and we are here from activating start scene.
+			// The problem is: we should get to main menu state immediately.
+			// That is why we calling SendMainMenu here.
+			scene := MainMenuScene{}
+			scene.SendMainMenu(message)
+		}
 	})
 }
